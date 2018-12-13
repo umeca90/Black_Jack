82a8f7e72f2c310837c08c9a0b84f983612016d6
umeca90 <umeca90@gmail.com>
2018-12-13 13:49:58 +0000

    spliting logic between interface and game

--- main.rb
+++ main.rb
@@ -9,18 +9,4 @@ require_relative "game"
 require_relative "game_interface"
 require_relative "validation"
 
-# class Main
-# puts
-
-# begin
-# name = gets.chomp.to_s.capitalize
-# player = Player.new(name)
-# croupier = Croupier.new("Cotton_Eye_Joe")
-# rescue StandardError => e
-# puts e.to_s
-# retry
-# end
 Game.new
-
-# GameInterface.new(game)
-# end
